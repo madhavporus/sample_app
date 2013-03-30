20a91854f691e49984356d50f4d44067a05a9b47
Madhav <maalla@cisco.com>
2013-03-29 18:46:30 -0700

    Sample App updates - ver 1.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'b29b712d9d9222032e574e17a64d0c9aff969a4b77c2de010a70b7c332157f0220cb829a35578751611429e6eefdfb7495d75113cd46dabfaa4a5ef8fc23c96a'
